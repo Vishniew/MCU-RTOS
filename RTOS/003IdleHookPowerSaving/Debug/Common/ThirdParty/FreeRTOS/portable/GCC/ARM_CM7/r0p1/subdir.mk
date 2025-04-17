6e1973c7b65334180207641d4abf5e7304627e69
Vishniew <vishnukesari14@gmail.com>
2025-04-17 20:14:20 +0530

    Idle hook exercise is added

---
+++ RTOS/003IdleHookPowerSaving/Debug/Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/packetp/WS/RTOS_WS/Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.c 
+
+OBJS += \
+./Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.o 
+
+C_DEPS += \
+./Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.o: /home/packetp/WS/RTOS_WS/Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.c Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F746xx -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I/home/packetp/WS/RTOS_WS/Common/ThirdParty/FreeRTOS/include -I/home/packetp/WS/RTOS_WS/Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1 -I/home/packetp/WS/RTOS_WS/Common/ThirdParty/SEGGER/Config -I/home/packetp/WS/RTOS_WS/Common/ThirdParty/SEGGER/OS -I/home/packetp/WS/RTOS_WS/Common/ThirdParty/SEGGER/SEGGER -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Common-2f-ThirdParty-2f-FreeRTOS-2f-portable-2f-GCC-2f-ARM_CM7-2f-r0p1
+
+clean-Common-2f-ThirdParty-2f-FreeRTOS-2f-portable-2f-GCC-2f-ARM_CM7-2f-r0p1:
+	-$(RM) ./Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.cyclo ./Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.d ./Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.o ./Common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM7/r0p1/port.su
+
+.PHONY: clean-Common-2f-ThirdParty-2f-FreeRTOS-2f-portable-2f-GCC-2f-ARM_CM7-2f-r0p1
+
